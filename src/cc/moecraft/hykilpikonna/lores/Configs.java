faa8dd3ccafbc73b31aa109924eb760327e0e020
kilpikonna <871674895@qq.com>
2017-06-11 10:20:09 -0400

    Changed 1_7 to 1.7

--- src/cc/moecraft/hykilpikonna/lores/Configs.java
+++ src/cc/moecraft/hykilpikonna/lores/Configs.java
@@ -52,7 +52,7 @@ public class Configs
         config.set("ConfigVersion", Bukkit.getPluginManager().getPlugin("HyLores").getDescription().getVersion());
 
         //API版本
-        config.addDefault("API.UsePlayEffectAPIInsteadOfParticleLib", Bukkit.getVersion().contains("1_7"));
+        config.addDefault("API.UsePlayEffectAPIInsteadOfParticleLib", Bukkit.getVersion().contains("1.7"));
         loglogger.Debug(String.format("[加载]当前服务器版本为%s", Bukkit.getVersion()));
 
         //心心特效
