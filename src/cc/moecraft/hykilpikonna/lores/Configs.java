07aeca6111cbe8ebcc71fb0463535889b948fd83
kilpikonna <871674895@qq.com>
2017-06-10 20:31:05 -0400

    Fixed stuff

--- src/cc/moecraft/hykilpikonna/lores/Configs.java
+++ src/cc/moecraft/hykilpikonna/lores/Configs.java
@@ -2,6 +2,8 @@ package cc.moecraft.hykilpikonna.lores;
 
 import org.bukkit.configuration.Configuration;
 
+import static cc.moecraft.hykilpikonna.lores.HyLores.loglogger;
+
 /**
  * 此类由 Hykilpikonna 在 2017/06/10 创建!
  * Created by Hykilpikonna on 2017/06/10!
@@ -13,6 +15,7 @@ public class Configs
     static Configuration config = HyLores.getInstance().getConfig();
     static void checkConfig()
     {
+        loglogger.Debug("[加载]正在检查配置");
         if (config.getBoolean("DefaultConfig"))
         {
 
--- src/cc/moecraft/hykilpikonna/lores/Setup.java
+++ src/cc/moecraft/hykilpikonna/lores/Setup.java
@@ -1,8 +1,10 @@
 package cc.moecraft.hykilpikonna.lores;
 
 import cc.moecraft.hykilpikonna.lores.Listeners.Effects.AttackEffectListener;
+import jdk.nashorn.internal.runtime.Debug;
 
 import static cc.moecraft.hykilpikonna.lores.Configs.checkConfig;
+import static cc.moecraft.hykilpikonna.lores.HyLores.loglogger;
 
 /**
  * 此类由 Hykilpikonna 在 2017/06/10 创建!
@@ -14,6 +16,7 @@ public class Setup
 {
     public static void setup()
     {
+        loglogger.Debug("[加载]正在运行Setup");
         checkConfig();
         AttackEffectListener attackEffectListener = new AttackEffectListener();
     }
